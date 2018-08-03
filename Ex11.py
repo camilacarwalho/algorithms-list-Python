9d590daf8b71cce4881997569fb44b82c4804039
camiula <camilacarwalho3@gmail.com>
2018-08-03 05:25:24 -0300

    4th

---
+++ Ex11.py
@@ -0,0 +1,4 @@
+real = input('Insira o valor em reais')
+cotacao = input('Insira a cotacao atual do dolar')
+dolar = float(real)*float(cotacao)
+print "Valor em Dolares = %.2f US" % dolar
---
+++ Ex12.py
@@ -0,0 +1,3 @@
+pol = input('Insira um valor em polegadas')
+cm = float(pol)*2.54
+print "Valor em cm = %.2f " % cm
