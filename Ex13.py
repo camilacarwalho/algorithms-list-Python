fb282b83b91da0c390b27c158b52928789466d9f
camiula <camilacarwalho3@gmail.com>
2018-08-03 05:49:56 -0300

    4th

---
+++ Ex13.py
@@ -0,0 +1,6 @@
+kb = input('Insira um valor em KB')
+byte = float(kb)*1024
+bits = float(byte)*8
+print "Valorem em:"
+print "Byte = %.2f " % byte
+print "Bits= %.2f" % bits
---
+++ Ex14.py
@@ -0,0 +1,3 @@
+test = input("Insira uma palavra\n Dica : Use aspas\n") 
+comprimento = len(test)
+print "Comprimento da palavra=",comprimento
